79c8c35bbdf1daaa058c8cd00f7a454fac47fdd6
Brian Pawlik <kilwap147@gmail.com>
2020-01-08 21:00:36 -0600

    First commit. Hello World!

--- setup-git-v1.sh
+++ setup-git-v1.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -e
-##################################################################################################################
-# Author	:	Erik Dubois
-# Website	:	https://www.erikdubois.be
-# Website	:	https://www.arcolinux.info
-# Website	:	https://www.arcolinux.com
-# Website	:	https://www.arcolinuxd.com
-# Website	:	https://www.arcolinuxb.com
-# Website	:	https://www.arcolinuxiso.com
-# Website	:	https://www.arcolinuxforum.com
-##################################################################################################################
+####################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
-##################################################################################################################
+####################################################################################################
 # change into your name and email.
 
 git init
-git config --global user.name "Erik Dubois"
-git config --global user.email "erik.dubois@gmail.com"
+git config --global user.name "Brian Pawlik"
+git config --global user.email "kilwap147@gmail.com"
 sudo git config --system core.editor nano
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=25000'
